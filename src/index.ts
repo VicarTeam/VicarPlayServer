a5fa2d7c448a1c0b0360b4dd5b2391992239326d
DasDarki <rageskilln@gmail.com>
2022-07-03 17:42:54 +0200

    add base express application

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
-console.log("Hello World");
+require('dotenv').config();
 
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
 
+const app = express();
+
+app.use(express.json());
+app.use(helmet());
+app.use(cors({
+    origin: process.env.CORS
+}));
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
